adc311dae7630e60fb8cc0a9a652c4652276b74f
Mark Kulik <kulik_mark@mail.ru>
2022-02-11 18:09:51 +0300

    Initial Commit

---
+++ Talks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talks
+//
+//  Created by Марк Кулик on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
